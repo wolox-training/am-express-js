7ab8d29e987d7a00616bfdc09672b261da9e9c09
Alexis Moragues <alexis.moragues@wolox.com.ar>
2018-07-11 12:41:19 -0300

    add albums database

--- app/controllers/albums.js
+++ app/controllers/albums.js
@@ -1,4 +1,5 @@
 const User = require('../models').user,
+  albums = require('../models').albums,
   logger = require('../logger'),
   fetch = require('node-fetch'),
   sessionsManager = require('../services/sessionsManager'),
@@ -10,5 +11,24 @@ exports.listAlbums = (req, res, next) => {
     .then(json => {
       logger.info('User requested albums and received album list');
       res.status(200).send(json);
+    });
+};
+
+exports.buyAlbum = (req, res, next) => {
+  const decoded = sessionsManager.decode(req.headers.authorization);
+
+  return User.findOne({ where: { email: decoded.email } })
+    .then(user => {
+      console.log(user.dataValues.id);
+      const sale = {
+        userId: user.dataValues.id,
+        albumId: req.params.id
+      };
+      return albums.createModel(sale).then(newSale => {
+        res.status(200).send({ sale: newSale });
+      });
+    })
+    .catch(err => {
+      next(err);
     });
 };
---
+++ app/models/albums.js
@@ -0,0 +1,43 @@
+const logger = require('../logger'),
+  errors = require('../errors');
+
+module.exports = (sequelize, DataTypes) => {
+  const albums = sequelize.define(
+    'albums',
+    {
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: 'user_id'
+      },
+      albumId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: 'album_id'
+      }
+    },
+    {
+      underscored: true
+    }
+  );
+
+  albums.associate = function(models) {
+    // associations can be defined here
+  };
+
+  albums.createModel = sale => {
+    albums
+      .findOne({ where: { userId: sale.userId, albumId: sale.albumId } })
+      .then(alreadyBought => {
+        if (alreadyBought) {
+          throw errors.alreadyBought(sale.userId, sale.albumId);
+        } else {
+          return albums.create(sale);
+        }
+      })
+      .catch(error => {
+        logger.error('Already bought');
+      });
+  };
+  return albums;
+};
