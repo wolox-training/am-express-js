c22ee824e7a130ddc9424abad71c4af5bce4c98e
Alexis Moragues <alexis.moragues@wolox.com.ar>
2018-07-12 11:39:08 -0300

    Added tested variable

--- config/testing.js
+++ config/testing.js
@@ -5,6 +5,7 @@ exports.config = {
     database: {
       name: process.env.NODE_API_DB_NAME_TEST
     },
+    albumList: process.env.ALBUM_LIST_URL,
     session: {
       secret: 'some-super-secret'
     }
