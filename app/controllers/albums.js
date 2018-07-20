6d2984f404b24365cdba35f499e90ead8f72a51c
Alexis Moragues <alexis.moragues@wolox.com.ar>
2018-07-20 15:59:03 -0300

    replace findAll for findOne and takes out Promise.all

--- app/controllers/albums.js
+++ app/controllers/albums.js
@@ -9,10 +9,12 @@ const User = require('../models').user,
   errors = require('../errors');
 
 exports.listAlbums = (req, res, next) => {
+  const albumsList = [];
   albumFetcher
-    .listAlbums()
-    .then(albumList => {
-      res.status(200).send({ albums: albumList });
+    .listAlbums(albumsList)
+    .then(albums => {
+      logger.info('Showed albums list');
+      res.status(200).send({ albums });
     })
     .catch(error => {
       logger.error('Could not find list of albums bought');
@@ -55,16 +57,15 @@ exports.showAlbumsBought = (req, res, next) => {
 };
 
 exports.showAlbumPhotos = (req, res, next) => {
-  return Album.findAll({ where: { userId: req.user.id, albumId: req.params.id } })
-    .then(purchases => {
-      const promises = purchases.map(element => {
-        return albumFetcher.getAlbumPhotoById(element.albumId);
-      });
-      return Promise.all(promises).then(albumsBought => {
-        logger.info('Showed album photos correctly');
-        res.status(200);
-        res.send({ albums: albumsBought });
-      });
+  return Album.findOne({ where: { userId: req.user.id, albumId: req.params.id } })
+    .then(album => {
+      if (album) {
+        return albumFetcher.getAlbumPhotoById(req.params.id).then(photos => {
+          logger.info('Showed list of photos');
+          res.status(200);
+          res.send({ photos });
+        });
+      }
     })
     .catch(error => {
       logger.error('Could not find list of albums photos');
